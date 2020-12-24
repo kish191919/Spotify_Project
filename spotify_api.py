3f5ebef572f551281e52349025003a9f3a74e6b7
“Sunghwan <Sunghwan.ki@bbva.com>
2020-12-24 09:00:46 -0600

    Add pickle file

--- spotify_api.py
+++ spotify_api.py
@@ -5,12 +5,61 @@ import requests
 import base64
 import json
 import logging
+import pickle
 
-client_id = 
-client_secret =
+
+# make pickle file
+# pickle.dump(' ', open("./client_id.plk","wb"))
+client_id = "1"
+# client_id = pickle.load(open('./client_id.plk','rb'))
+client_secret = pickle.load(open('./client_secret.plk','rb'))
 
 
 def main():
+    headers = get_headers(client_id,client_secret)
+    # print(headers)
+
+    # Spotify Search api
+    params = {
+        "q":"BTS",
+        "type":"artist",
+        "limit": "1"  # limit the information
+        }
+
+    # r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+    # print(r.status_code)
+    # print(r.text)
+    # print(r.headers)
+    # sys.exit(0)
+
+    try:
+        r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+    except:
+        sys.exit(0)
+        if r.status_code != 200:
+            logging.error(json.loads(r.text))
+            # logging.error(r.text)
+
+
+            if r.status_code == 429:
+
+                retry_after = json.loads(r.headers)['Retry-After']
+                time.sleep(int(retry_after))
+
+                r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+            # access_token expired
+            elif r.status_code == 401:
+
+                headers = get_headers(client_id, client_secret)
+                r = requests.get("https://api.spotify.com/v1/search", params=params, headers=headers)
+
+            else:
+                sys.exit(1)
+
+
+def get_headers(client_id, client_secret):
     endpoint = "https://accounts.spotify.com/api/token"
 
     # python3 add .encode('utf-8')).decode('ascii')
@@ -26,15 +75,27 @@ def main():
 
     r = requests.post(endpoint, data=payload, headers=headers)
 
+    # Check the status of access token
+    # print(r.status_code)
+    # print(r.headers)
+    # print(r.text)
+    # print(type(r.text))
+    # sys.exit(0)
+
+    # text is <class 'str'> so we need to convert it as dictionary type
+    # output :  <class 'dict'>
+    # access_token = json.loads(r.text)
+    # print(type(access_token))
+    # print(access_token)
+    # sys.exit(0)
+
     access_token = json.loads(r.text)['access_token']
 
-    headers ={
+    headers = {
+        "Authorization": "Bearer {}".format(access_token)
+        }
 
-    }
-
-
-    print('fastcampus')
-
+    return headers
 
 
 
